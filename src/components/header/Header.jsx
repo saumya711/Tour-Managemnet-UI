9a7fcb8cf0ec2348d951e76f69e85186475c2ea9
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-06-19 20:56:51 +0530

    CHG | Create Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { container, Row, Button, Container } from 'reactstrap';
+import { NavLink, Link  } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import './Header.css'
+
+const nav_links = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/about',
+    display: 'About'
+  },
+  {
+    path: '/tours',
+    display: 'Tours'
+  },
+]
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className='header'>
+      <Container>
+        <Row>
+          <div className='nav_wrapper d-flex align-items-center justify-center-between'>
+            <div className="logo">
+              <img src={logo} alt=''/>
+            </div>
+
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav_links.map((item, index) => (
+                  <li className="nav_item" key={index}>
+                    <NavLink 
+                      to={item.path}
+                      className={navClass => navClass.isActive ? "active_link" : ""}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="nav_right d-flex align-items-center gap-4">
+              <div className="nav_btns d-flex align-items-center gap-4">
+                <Button className='btn secondary__btn'>
+                  <Link to='/login'>Login</Link>
+                </Button>
+                <Button className='btn primary__btn'>
+                  <Link to='/register'>Register</Link>
+                </Button>
+              </div>
+            </div>
+
+            <span className="mobile_menu">
+              <i class="ri-menu-line"></i>
+            </span>
+
+          </div>
+        </Row>
+      </Container>
+    </header>
   )
 }
 
