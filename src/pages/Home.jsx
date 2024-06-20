b52d0a443ca2b26ada7a32e3e8be652944f5924b
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-06-20 23:18:09 +0530

    CHG | Created Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css';
+
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg2 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import worldImg from '../assets/images/world.png';
+import Subtitle from '../shared/Subtitle';
+
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className="hero_content">
+                <div className="hero_subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt=''/>
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className='highlight'>memories</span>
+                </h1>
+                <p>
+                Welcome to TourMaster, your ultimate travel companion for seamless 
+                and unforgettable adventures. Effortlessly plan, book, 
+                and manage your tours with personalized recommendations and curated experiences. 
+                Your journey to amazing memories starts here!
+                </p>
+              </div>
+            </Col>
+
+            <Col lg='2'>
+              <div className="hero-img-box">
+                <img src={heroImg} alt='' />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero-img-box mt-4">
+                <img src={heroVideo} alt='' />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero-img-box mt-5">
+                <img src={heroImg2} alt='' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
